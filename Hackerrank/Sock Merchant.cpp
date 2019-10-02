25d74d7f3569ff2189744d7fea63017af62337d2
jitendrajat10099 <44499868+jitendrajat10099@users.noreply.github.com>
2019-10-02 11:45:51 +0530

    Update Sock Merchant.cpp

--- Hackerrank/Sock Merchant.cpp
+++ Hackerrank/Sock Merchant.cpp
@@ -1,6 +1,9 @@
+/*
 Problem Link: https://www.hackerrank.com/challenges/sock-merchant/copy-from/114501869
-#include <bits/stdc++.h>
 
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
