c5074372e628e2ef22a1a4dc958e2fd6578c5d83
Sreesruthi P K <31540953+massg@users.noreply.github.com>
2019-10-12 09:22:57 +0530

    Solution of Jumping_on_the_clouds

---
+++ Hackerrank/Jumping_on_the_clouds.c
@@ -0,0 +1,120 @@
+// Link: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the jumpingOnClouds function below.
+int jumpingOnClouds(int c_count, int* c) {
+    int jump=0;
+    int curr=0;
+    while(curr+1!=c_count){
+        if(c[curr+2]==0){
+            jump++;
+            curr+=2;
+        }
+        else if(c[curr+1]==0){
+            jump++;
+            curr++;
+        }
+        else
+            break;
+    }
+return jump;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** c_temp = split_string(readline());
+
+    int* c = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        char* c_item_endptr;
+        char* c_item_str = *(c_temp + i);
+        int c_item = strtol(c_item_str, &c_item_endptr, 10);
+
+        if (c_item_endptr == c_item_str || *c_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        *(c + i) = c_item;
+    }
+
+    int c_count = n;
+
+    int result = jumpingOnClouds(c_count, c);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
