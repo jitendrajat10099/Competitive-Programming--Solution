3f4e0d7e871428f9461345d5a1795b8665b7d312
ed-archer <56473062+ed-archer@users.noreply.github.com>
2019-10-13 15:08:37 +0530

    Add files via upload

---
+++ Hackerearth/Basics of Implementation/Palindromic String.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#include <string.h>
+
+void main()
+
+{
+
+	int x,v;
+
+	v=0;
+
+	char S[100];
+
+	scanf("%s",S);
+
+	x=strlen(S)-1;
+
+	for (int i=0; i<=x; i++)
+
+	{
+
+		if (S[i]==S[x])
+
+		{
+
+			v=v;
+
+		}
+
+		else
+
+		{
+
+			v++;
+
+		}
+
+		x--;
+
+	}
+
+	if (v==0)
+
+	{
+
+		printf("YES\n");
+
+	}
+
+	else
+
+	{
+
+		printf("NO\n");
+
+	}
+
+}
---
+++ Hackerearth/Basics of Implementation/Read from STDIN, Write to STDOUT.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int a;
+
+    char s[15];
+
+    cin>>a>>s;
+
+    cout<<a*2<<'\n'<<s;
+
+}
